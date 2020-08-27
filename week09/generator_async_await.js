e08f5294e0a751698a476ea3d871012e2de8a30f
zjl <jialuzhang805@gmail.com>
2020-08-28 00:11:38 +0800

    迭代器生成器实现async/await效果

--- week09/generator_async_await.js
+++ week09/generator_async_await.js
@@ -1,44 +1,57 @@
 
-// let sleep = (sleepTime) => {
-//     console.log("sleepTime");
-//     return new Promise((resolve, reject) => {
-//         setTimeout(resolve, sleepTime);
-//     });
-// };
-// let lights = document.getElementsByClassName("ll");
-// let yellowLight = document.querySelector("#yellow");
-// let greenLight = document.querySelector("#green");
-// let redLight = document.querySelector("#red");
-// function green() {
-//     Array.from(lights).forEach((element) => {
-//         element.className = "ll";
-//     });
-//     greenLight.classList.add("green");
-// }
+let sleep = (sleepTime) => {
+    console.log("sleepTime");
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, sleepTime);
+    });
+};
+let lights = document.getElementsByClassName("ll");
+let yellowLight = document.querySelector("#yellow");
+let greenLight = document.querySelector("#green");
+let redLight = document.querySelector("#red");
+function green() {
+    Array.from(lights).forEach((element) => {
+        element.className = "ll";
+    });
+    greenLight.classList.add("green");
+}
 
-// function yellow() {
-//     Array.from(lights).forEach((element) => {
-//         element.className = "ll";
-//     });
-//     yellowLight.classList.add("yellow");
-// }
+function yellow() {
+    Array.from(lights).forEach((element) => {
+        element.className = "ll";
+    });
+    yellowLight.classList.add("yellow");
+}
 
-// function red() {
-//     Array.from(lights).forEach((element) => {
-//         element.className = "ll";
-//     });
-//     redLight.classList.add("red");
-// }
+function red() {
+    Array.from(lights).forEach((element) => {
+        element.className = "ll";
+    });
+    redLight.classList.add("red");
+}
 
 
 function* generator() {
-    let count = 10;
-    while(count) {
-        yield count --;
+    while(true) {
+        red();
+        yield sleep(1000);
+        yellow();
+        yield sleep(2000);
+        green();
+        yield sleep(3000);
     }
 }
 
-let it = generator();
-for (let i of [1,2,3]) {
-    console.log(i, it.next().value);
+function run(iterator) {
+    let {value, done} = iterator.next();
+    if (value instanceof Promise) {
+        value.then(() => {
+            run(iterator);
+        })
+    }
 }
+run(generator());
+// let it = generator();
+// for (let i of [1,2,3]) {
+//     console.log(i, it.next().value);
+// }
