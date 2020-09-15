457cd79909dbbd49f8179f28436c4b1bf625affb
zjl <jialuzhang0805@gmail.com>
2020-09-15 22:34:59 +0800

    then方法实现返回promise

--- week09/MyPromise.js
+++ week09/MyPromise.js
@@ -34,13 +34,79 @@ class MyPromise {
     }
 
     then(onFulfilled, onRejected) {
-        // if (this.state === FULFILLED) {
-        //     onFulfilled(this.value);
-        // }else if (this.state === REJECTED) {
-        //     onRejected(this.reason);
-        // }
-        this.onFullfilledCallbacks.push(onFulfilled)
-        this.onRejectedCallbacks.push(onRejected)
+        const promise2 = new MyPromise((resolve, reject) => {
+            if (this.state === PENDING) {
+                this.onFullfilledCallbacks.push(() => {
+                    try {
+                        let x = onFulfilled(this.value);
+                        this.resolvePromise(promise2, x, resolve, reject);
+                    }catch(error) {
+                        reject(error);
+                    }
+                })
+
+                this.onRejectedCallbacks.push(() => {
+                    try {
+                        let x = onRejected(this.reason);
+                        this.resolvePromise(promise2, x, resolve, reject);
+                    }catch(error) {
+                        reject(error)
+                    }
+                })
+            }
+            else if (this.state === FULFILLED) {
+                try {
+                    let x = onFulfilled(this.value);
+                    this.resolvePromise(promise2, x, resolve, reject);
+                }catch (error) {
+                    reject(error)
+                }
+            }
+            else if (this.state === REJECTED) {
+                try {
+                    let x = onRejected(this.value);
+                    this.resolvePromise(promise2, x, resolve, reject);
+                }catch (error) {
+                    reject(error)
+                }
+            }
+        })
+
+        return promise2;
+    }
+
+    resolvePromise(promise2, x, resolve, reject) {
+        if (promise2 === x) {
+            reject(new TypeError('Chaining Cycle'));
+        }
+        if (x && typeof x === 'object' || typeof x === 'function') {
+            let used;
+            try {
+                let then = x.then;
+                // x.then 是 function 说明 x 是promise
+                if (typeof then === 'function') {
+                    then.call(x, (y) => {
+                        if (used) return;
+                        used = true;
+                        resolvePromise(promise2, y, resolve, reject)
+                    }, (r) => {
+                        if (used) return;
+                        used = true;
+                        reject(r);
+                    })
+                } else {
+                    if (used) return;
+                    used = true;
+                    resolve(x);
+                }
+            }catch (error) {
+                if (used) return;
+                used = true
+                reject(error)
+            }
+        } else {
+            resolve(x);
+        }
     }
 
 }
