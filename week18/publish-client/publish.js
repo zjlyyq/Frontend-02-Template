98dc02240cdad68b3a2b4421369239fce1940d85
tk_zjl <016760@zjtlcb.com>
2020-11-06 22:50:36 +0800

    sync

--- week18/publish-client/publish.js
+++ week18/publish-client/publish.js
@@ -2,32 +2,38 @@ let http = require('http');
 const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
-const archive = archiver('zip', {
-    zlib: { level: 9 }
-});
+const child_process = require('child_process');
 
-archive.directory(path.resolve(__dirname, '../sample/'), false);
-archive.finalize();
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.737cbe94d227d18b`);
 
-fs.stat(path.resolve(__dirname, '../sample/sample.html'), function(err, stats) {
-    if (err) throw err;
-    console.log(stats);
-    let request = http.request({
-        hostname: "121.199.20.52",
-        port: 8089,
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-        }
-    }, res => {
-        console.log(res.toString());
-    })
-    archive.pipe(request);
-    archive.on('error', function(err) {
-        throw err;
+function publish() {
+    const archive = archiver('zip', {
+        zlib: { level: 9 }
     });
-    archive.on('end', () => {
-        console.log('pipe end');
-        request.end();
+    
+    archive.directory(path.resolve(__dirname, '../sample/'), false);
+    archive.finalize();
+    
+    fs.stat(path.resolve(__dirname, '../sample/sample.html'), function(err, stats) {
+        if (err) throw err;
+        console.log(stats);
+        let request = http.request({
+            hostname: "121.199.20.52",
+            port: 8089,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/octet-stream',
+            }
+        }, res => {
+            console.log(res.toString());
+        })
+        archive.pipe(request);
+        archive.on('error', function(err) {
+            throw err;
+        });
+        archive.on('end', () => {
+            console.log('pipe end');
+            request.end();
+        })
     })
-})
+}
--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,13 +1,59 @@
 let http = require('http');
+let https = require('https');
 let fs = require('fs');
 let path = require('path');
 let unzipper = require('unzipper');
+const queryString = require('querystring');
 
-console.log(path.resolve(__dirname, '../public/index.html'));
-http.createServer(function (req, res) {
-    console.log(req.headers);
-    req.pipe(unzipper.Extract({ path: path.resolve(__dirname, '../public/') }));
-    req.on('end', () => {
-        res.end('Succeed');
+function getAccessToken(code, cb) {
+    console.log(`/login/oauth/access_token?code=${code}&client_id=Iv1.737cbe94d227d18b&client_secret=290beacc9ede444f8694ae201e988eaea9dc065a`)
+    let request = 
+    https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.737cbe94d227d18b&client_secret=290beacc9ede444f8694ae201e988eaea9dc065a`,
+        method: 'POST',
+        port: 443
+    }, function(response) {
+        let body = "";
+        response.on('data', chunk => {
+            console.log(chunk.toString());
+            body += chunk.toString();
+        })
+
+        response.on('end', chunk => {
+            if (chunk) body += chunk;
+            let o = queryString.parse(body);
+            cb(o);
+        })
+        // console.log(response);
     })
+
+    request.end();
+}
+
+// auth 路由： 接受code ,post(Parameters: code + client_id + client_secret)请求换取access_token
+function auth(request, response) {
+    const params = request.url.match(/^\/auth\?([\s\S]+)$/);
+    const query = queryString.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    
+    let code = query['code'];
+    if (code) getAccessToken(code, function(userInfo){
+        response.end(JSON.stringify(userInfo));
+        console.log(userInfo);
+    });
+    // response.end('auth');
+}
+http.createServer(function (req, res) {
+    // console.log(req.headers);
+    if (req.url.match(/^\/auth\?/)){
+        auth(req, res);
+    }
+    else {
+        if (req.url === '/favicon.ico') return;
+        console.log(req.url)
+        req.pipe(unzipper.Extract({ path: path.resolve(__dirname, '../public/') }));
+        req.on('end', () => {
+            res.end('Succeed');
+        })
+    } 
 }).listen(8089);
