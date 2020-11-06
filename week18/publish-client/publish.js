54edafc2eb1b981c6a2975c2feb63f0ae8165461
jialuzhang <jialuzhang0805@gmail.com>
2020-11-07 02:51:53 +0800

    用GitHub oAuth做一个登录实例

--- week18/publish-client/publish.js
+++ week18/publish-client/publish.js
@@ -3,10 +3,30 @@ const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
 const child_process = require('child_process');
+const queryString = require('querystring');
 
 child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.737cbe94d227d18b`);
 
-function publish() {
+let server = http.createServer(function (request, response) {
+    if (request.url.match(/^\/auth\?/)){
+        
+    }
+    else {
+        if (request.url === '/favicon.ico') return;
+        const query = queryString.parse(request.url.match(/^\/\?([\s\S]+)$/)[1]);
+        let token = query['token'];
+        console.log(token);
+        // response.end(token);
+        publish(token, function() {
+            response.end('publish succeed!');
+        });
+    } 
+})
+server.listen(8090)
+// listen(8090);
+
+
+function publish(token, cb) {
     const archive = archiver('zip', {
         zlib: { level: 9 }
     });
@@ -14,26 +34,25 @@ function publish() {
     archive.directory(path.resolve(__dirname, '../sample/'), false);
     archive.finalize();
     
-    fs.stat(path.resolve(__dirname, '../sample/sample.html'), function(err, stats) {
-        if (err) throw err;
-        console.log(stats);
-        let request = http.request({
-            hostname: "121.199.20.52",
-            port: 8089,
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/octet-stream',
-            }
-        }, res => {
-            console.log(res.toString());
-        })
-        archive.pipe(request);
-        archive.on('error', function(err) {
-            throw err;
-        });
-        archive.on('end', () => {
-            console.log('pipe end');
-            request.end();
-        })
+    let request = http.request({
+        // hostname: "121.199.20.52",
+        hostname: 'localhost',
+        port: 8089,
+        method: 'post',
+        path: '/publish?token=' + token,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+        }
+    }, res => {
+        console.log(res.toString());
     })
+    archive.pipe(request);
+    archive.on('error', function(err) {
+        throw err;
+    });
+    archive.on('end', () => {
+        console.log('pipe end');
+        request.end();
+        cb();
+    })   
 }
--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -4,6 +4,7 @@ let fs = require('fs');
 let path = require('path');
 let unzipper = require('unzipper');
 const queryString = require('querystring');
+const { use } = require('../express-generator-app/routes');
 
 function getAccessToken(code, cb) {
     console.log(`/login/oauth/access_token?code=${code}&client_id=Iv1.737cbe94d227d18b&client_secret=290beacc9ede444f8694ae201e988eaea9dc065a`)
@@ -31,6 +32,36 @@ function getAccessToken(code, cb) {
     request.end();
 }
 
+function getUserInfo(token, cb) {
+    console.log({
+        Authorization: `token ${token}`,
+        // User-Agent: 'zjl-toy-publish'
+    })
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: `/user`,
+        method: 'GET',
+        headers: {
+            Authorization: `token ${token}`,
+            "User-Agent": 'zjl-toy-publish'
+        },
+        port: 443
+    }, function(response) {
+        let body = "";
+        response.on('data', chunk => {
+            // console.log(chunk.toString());
+            body += chunk.toString();
+        })
+
+        response.on('end', chunk => {
+            if (chunk) body += chunk;
+            
+            cb(JSON.parse(body));
+        })
+        // console.log(response);
+    })
+    request.end();
+}
 // auth 路由： 接受code ,post(Parameters: code + client_id + client_secret)请求换取access_token
 function auth(request, response) {
     const params = request.url.match(/^\/auth\?([\s\S]+)$/);
@@ -38,15 +69,38 @@ function auth(request, response) {
     
     let code = query['code'];
     if (code) getAccessToken(code, function(userInfo){
-        response.end(JSON.stringify(userInfo));
+        response.end(`<a href="http://localhost:8090?token=${userInfo.access_token}">publish</a>`)
         console.log(userInfo);
     });
-    // response.end('auth');
 }
+
+// publish
+function publish(request, response) {
+    const params = request.url.match(/^\/publish\?([\s\S]+)$/);
+    const query = queryString.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1]);
+    const token = query.token;
+
+    console.log('token: ' + token);
+    getUserInfo(token, function(userInfo) {
+        console.log(userInfo)
+        if (userInfo.login === 'zjlyyq') {
+            request.pipe(unzipper.Extract({ path: path.resolve(__dirname, '../public/') }));
+            request.on('end', () => {
+                response.end('Succeed!');
+            })
+        }else {
+            response.end('sorry, you do are forbidden for this operation.')
+        }
+    });
+}
+
 http.createServer(function (req, res) {
-    // console.log(req.headers);
+    console.log(req.url);
     if (req.url.match(/^\/auth\?/)){
         auth(req, res);
+    }
+    else if (req.url.match(/^\/publish\?/)) {
+        publish(req, res);
     }
     else {
         if (req.url === '/favicon.ico') return;
